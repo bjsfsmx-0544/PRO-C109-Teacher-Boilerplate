2d3d0b20b4463b664af8ce11c7dfb1b6a1ae31b3
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-03-04 13:32:23 -0600

    Traducción 1.0
    Modificación de comentarios y elementos en pantalla. Actualización de README.md

--- virtual_keyboard.py
+++ virtual_keyboard.py
@@ -14,54 +14,54 @@ hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5
 tipIds = [4, 8, 12, 16, 20]
 
 
-# Define a function to count fingers
+# Definir una función para contar dedos
 def countFingers(image, hand_landmarks, handNo=0):
 
     if hand_landmarks:
-        # Get all Landmarks of the FIRST Hand VISIBLE
+        # Obtener todas las marcas de referencia en la primera mano visible
         landmarks = hand_landmarks[handNo].landmark
 
-        # Count Fingers        
+        # Contar dedos
         fingers = []
 
         for lm_index in tipIds:
-                # Get Finger Tip and Bottom y Position Value
+                # Obtener los valores de la psosición "y" de la punta y parte inferior del dedo
                 finger_tip_y = landmarks[lm_index].y 
                 finger_bottom_y = landmarks[lm_index - 2].y
 
-                # Check if ANY FINGER is OPEN or CLOSED
+                # Verificar si algun dedo está abierto o cerrado
                 if lm_index !=4:
                     if finger_tip_y < finger_bottom_y:
                         fingers.append(1)
-                        # print("FINGER with id ",lm_index," is Open")
+                        # print("El dedo con ID ",lm_index," está abierto.")
 
                     if finger_tip_y > finger_bottom_y:
                         fingers.append(0)
-                        # print("FINGER with id ",lm_index," is Closed")
+                        # print("El dedo con ID ",lm_index," está cerrado.")
 
         totalFingers = fingers.count(1)
         
-        # PLAY or PAUSE a Video
+        # Reproducir o pausar un video
         
         ################################
 
-             # ADD CODE HERE #
+             # AGREGA CÓDIGO AQUÍ #
 
         ################################
         
 
-         # Move Video FORWARD & BACKWARDS 
+         # Mover un video hacia adelante o hacia atrás
          
          ################################
 
-             # ADD CODE HERE #
+             # AGREGA CÓDIGO AQUÍ #
 
         ################################ 
         
-# Define a function to 
+# Definir una función para
 def drawHandLanmarks(image, hand_landmarks):
 
-    # Darw connections between landmark points
+    # Dibujar conexiones entre las marcas de referencia
     if hand_landmarks:
 
       for landmarks in hand_landmarks:
@@ -75,21 +75,21 @@ while True:
 
     image = cv2.flip(image, 1)
     
-    # Detect the Hands Landmarks 
+    # Detectar las marcas de referencia de las manos
     results = hands.process(image)
 
-    # Get landmark position from the processed result
+    # Obtener las marcas de referencia del resultado procesado
     hand_landmarks = results.multi_hand_landmarks
 
-    # Draw Landmarks
+    # Dibujar las marcas de referencia
     drawHandLanmarks(image, hand_landmarks)
 
-    # Get Hand Fingers Position        
+    # Obtener la posoción de los dedos de las manos
     countFingers(image, hand_landmarks)
 
-    cv2.imshow("Media Controller", image)
+    cv2.imshow("Controlador de medios", image)
 
-    # Quit the window on pressing Sapcebar key
+    # Cerrar la ventana al presionar la barra espaciadora
     key = cv2.waitKey(1)
     if key == 27:
         break
